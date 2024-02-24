ce5a6c987c787197d5f9aed4612651fab42f0e9b
Orijadribo <danielorijadribo@gmail.com>
2024-02-24 22:34:25 +0300

    table

--- App/Screens/Cards/Table.jsx
+++ App/Screens/Cards/Table.jsx
@@ -1,4 +1,11 @@
-import { View, Text, StyleSheet, FlatList, TextInput } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  FlatList,
+  TextInput,
+  ScrollView,
+} from "react-native";
 import React from "react";
 
 export default function Table({ isFront }) {
@@ -26,58 +33,6 @@ export default function Table({ isFront }) {
         <Text style={styles.player}>Player 3</Text>
         <Text style={styles.player}>Player 4</Text>
       </View>
-      {/* Data  */}
-      <View style={styles.table}>
-        {/* Hole  */}
-        <FlatList
-          data={isFront ? holesFront : holesBack}
-          keyExtractor={(item) => item.toString()}
-          renderItem={({ item: hole, index }) => (
-            <View style={styles.table}>
-              <Text style={styles.holeParYards}>
-                {isFront ? holesFront[index] : holesBack[index]}
-              </Text>
-            </View>
-          )}
-        />
-
-        {/* Par  */}
-        <FlatList
-          data={isFront ? parsFront : parsBack}
-          keyExtractor={(item, index) => index.toString()}
-          renderItem={({ item: par, index }) => (
-            <View style={styles.table}>
-              <Text style={styles.holeParYards}>{par}</Text>
-            </View>
-          )}
-        />
-
-        {/* Yards  */}
-        <FlatList
-          data={isFront ? yardsFront : yardsBack}
-          keyExtractor={(item, index) => index.toString()}
-          renderItem={({ item: yards, index }) => (
-            <View style={styles.table}>
-              <Text style={styles.holeParYards}>{yards}</Text>
-            </View>
-          )}
-        />
-
-        {/* Player Inputs */}
-        {playerRows.map((player) => (
-          <View key={player}>
-            {[1, 2, 3, 4, 5, 6, 7, 8, 9].map((col) => (
-              <View key={col} style={styles.playerInputContainer}>
-                <TextInput
-                  style={styles.playerInput}
-                  keyboardType="numeric"
-                  // placeholder={`Player ${player}`}
-                />
-              </View>
-            ))}
-          </View>
-        ))}
-      </View>
     </View>
   );
 }
@@ -90,11 +45,6 @@ const styles = StyleSheet.create({
     padding: 10,
     marginTop: 20,
   },
-  table: {
-    padding: 10,
-    display: "flex",
-    flexDirection: "row",
-  },
   holeParYardsHeader: {
     width: "12%",
     display: "flex",
@@ -102,10 +52,6 @@ const styles = StyleSheet.create({
     alignItems: "center",
     textAlign: "center",
     justifyContent: "center",
-  },
-  holeParYards: {
-    width: "12%",
-    borderWidth: 2,
   },
   player: {
     width: "16%",
@@ -115,19 +61,37 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     textAlign: "center",
   },
+  table: {
+    // padding: 10,
+    display: "flex",
+    flexDirection: "row",
+    // justifyContent:'space-between',
+    borderWidth: 1,
+  },
+  tableCell: {
+    borderWidth: 1,
+    display: "flex",
+    flexDirection: "row",
+    width: "100%",
+  },
+  holeParYards: {
+    width: "100%",
+    // borderWidth: 1,
+    textAlign: "center",
+  },
   playerInputContainer: {
     flexDirection: "row",
     justifyContent: "space-between",
     // marginTop: 10,
   },
   playerInputCell: {
-    width: "16%",
-    borderWidth: 1,
-    padding: 5,
+    width: "100%",
+    // borderWidth: 1,
+    // padding: 5,
     textAlign: "center",
   },
   playerInput: {
-    width: "16%",
+    width: "100%",
     borderWidth: 1,
     padding: 5,
     textAlign: "center",
